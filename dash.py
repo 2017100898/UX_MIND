b3e03c573c3db04e8f2dcf8d59235e336e2eecae
HyeWon Park <2017100898@khu.ac.kr>
2023-08-27 23:26:48 +0900

    refactor: Fix some structure of dashboard

--- dash.py
+++ dash.py
@@ -2,25 +2,20 @@ import requests
 import streamlit as st
 
 st.set_page_config(
-    page_title="",
-    page_icon="🍄",
+    page_title="AIMS Dashboard",
+    page_icon="🚀",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
+reduce_header_height_style = """
+    <style>
+        div.block-container {padding-top:1rem;}
+    </style>
+"""
+st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
-# Create a form in the sidebar for users to input a sentence
-with st.sidebar:
-    st.title("AIMS XR Dashboard")
-
-    text_input = st.text_input("Enter condition of diffusion 👇", value = "")
-
-    if text_input:
-        requests.post("http://localhost:5000/diffusion_post_cmd", json={"text_input": text_input})
-
-
-cols = st.columns([1, 1])
-
+st.header("AIMS Dashboard")
 
 card_css = """
 <style>
@@ -36,18 +31,79 @@ card_css = """
     justify-content: center;
 }
 
+.card-title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.card-title > * + * {
+    margin-left: 5px;
+}
+
 .card-content {
     display: flex;
     flex-direction: column;
-    align-items: left;
-    text-align: left;
     color: white;
     font-size: 22px;
     font-weight: bold;
-    margin-bottom: 20px;
+}
+
+.tooltip-trigger {
+    position: relative;
+    cursor: pointer;
+    font-size: 11px;
+    border-radius: 50%; /* 100% 대신 50%로 변경 */
+    width: 19px; /* 원의 크기를 조정 */
+    height: 19px; /* 원의 크기를 조정 */
+    color: white; /* 텍스트 색상 설정 */
+    border-color: white;
+    border-style:solid;
+    border-width: 1.5px;
+    text-align: center; /* 텍스트 중앙 정렬 */
+    line-height: 19px; /* 텍스트 수직 중앙 정렬 */
+}
+
+.tooltip-trigger::before {
+    content: attr(data-tooltip);
+    position: absolute;
+    top: -30px;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 5px;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: white;
+    border-radius: 5px;
+    font-size: 14px;
+    white-space: nowrap;
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.2s, visibility 0.2s;
+}
+
+
+.tooltip-trigger:hover::before {
+    opacity: 1;
+    visibility: visible;
+}
+
+#transformSelect {
+            color: white;
+            border: 1px solid white;
+            background-color: transparent;
+            padding: 5px;
+            border-radius: 5px;
+        }
+
+#transformSelect option {
+    color: black;
+    border: 1px solid white;
 }
 </style>
 """
+
+
+cols = st.columns([1, 1])
 
 st.markdown(card_css, unsafe_allow_html=True)
 
@@ -57,41 +113,59 @@ with cols[0].container():
     with sub_cols_1[0].container():
         st.markdown('''
                 <div class="card">
-                    <div class="card-content">Pose Estimation</div>
-                    <iframe src="http://localhost:5000/pose_feed" width="100%" height="500" frameborder="0" scrolling="no">
+                    <div class="card-content">\t</div>
+                    <iframe src="http://localhost:5000/pose_feed" width="100%" height="490" frameborder="0" scrolling="no">
                     </iframe>
                 </div>''', unsafe_allow_html=True)
         
     with sub_cols_1[1].container():  
         st.markdown('''
+                    
                 <div class="card">
+                    <div class = "card-title" >
                     <div class="card-content">Emotion</div>
-                        <iframe src="http://localhost:5000/emotion_feed" width="100%" height="500" frameborder="0" scrolling="no">
+                    <div class="tooltip-trigger" data-tooltip="이곳에 표시될 내용을 입력하세요.">❔</div> 
+                    </div>
+                        <iframe src="http://localhost:5000/emotion_feed" width="100%" height="450" frameborder="0" scrolling="no">
                         </iframe>
                 </div>''', unsafe_allow_html=True)
     
     st.markdown('''
             <div class="card">
-                <div class="card-content">Diffusion Model</div>
-                    <iframe src="http://localhost:5000/diffusion_feed" width="100%" height="500" frameborder="0" scrolling="no">
-                    </iframe>
-            </div>''', unsafe_allow_html=True)
-
+                <div class = "card-title">
+                <div class="card-content">Transform into</div>
+                <select id="transformSelect">
+                    <option value="option1">Astronaut</option>
+                    <option value="option2">Spiderman</option>
+                    <option value="option3">Cat</option>
+                </select>
+                </div>
+                <iframe src="http://localhost:5000/diffusion_feed" width="100%" height="450" frameborder="0" scrolling="no">
+                </iframe>
+            </div>
+            ''', unsafe_allow_html=True)
+    
 with cols[1].container():
     st.markdown('''
             <div class="card">
-                <div class="card-content">EEG Stream</div>
-                    <iframe src="http://localhost:5000/eeg_feed" width="100%" height="500" frameborder="0" scrolling="no">
+                    <div class = "card-title">
+                    <div class="card-content">EEG Stream</div>
+                    <div class="tooltip-trigger" data-tooltip="이곳에 표시될 내용을 입력하세요.">❔</div> 
+                    </div>
+                    <iframe src="http://localhost:5000/eeg_feed" width="100%" height="550" frameborder="0" scrolling="no">
                     </iframe>
             </div>''', unsafe_allow_html=True)
     
-    sub_cols_2 = st.columns([1, 1])
+    sub_cols_2 = st.columns([1.2, 1])
 
     with sub_cols_2[0].container():
         st.markdown('''
                 <div class="card">
-                    <div class="card-content">Scalp Maps</div>
-                        <iframe src="http://localhost:5000/mne_feed" width="100%" height="500" frameborder="0" scrolling="no">
+                    <div class = "card-title">
+                    <div class="card-content">EEG Scalp Maps</div>
+                    <div class="tooltip-trigger" data-tooltip="이곳에 표시될 내용을 입력하세요.">❔</div> 
+                    </div>
+                        <iframe src="http://localhost:5000/mne_feed" width="100%" height="350" frameborder="0" scrolling="no">
                         </iframe>
                 </div>''', unsafe_allow_html=True)
     
@@ -99,8 +173,11 @@ with cols[1].container():
     with sub_cols_2[1].container():
         st.markdown('''
                 <div class="card">
-                    <div class="card-content" >Attention</div>
-                    <iframe src="http://localhost:5000/attention_feed" width="100%" height="500" frameborder="0" scrolling="no">
+                    <div class = "card-title">
+                    <div class="card-content">Attention</div>
+                    <div class="tooltip-trigger" data-tooltip="이곳에 표시될 내용을 입력하세요.">❔</div> 
+                    </div>
+                    <iframe src="http://localhost:5000/attention_feed" width="100%" height="350" frameborder="0" scrolling="no">
                         </iframe>
                 </div>''', unsafe_allow_html=True)
         
